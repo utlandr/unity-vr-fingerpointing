0caccb40591483c0fd199bb0288368fa8ecf46fa
Reed Bell <rbel068@aucklanduni.ac.nz>
2019-09-01 05:21:07 +1200

    An entire day wasted cause this doesn't work for some reason.

--- Assets/Scripts/Networking/PlayerUnit.cs
+++ Assets/Scripts/Networking/PlayerUnit.cs
@@ -20,5 +20,20 @@ public class PlayerUnit : NetworkBehaviour
                     componentsToDisable[i].enabled = false;
                 }
             }
+        
+        // So apparently the animation doesn't work unless you disable then 
+        // reenable. I am definitely doing something wrong but since it works
+        // im giving in to this shitty solution
+        else
+        {
+            // Disable
+            componentsToDisable[9].enabled = false;
+            componentsToDisable[10].enabled = false;
+
+            // Enable
+            componentsToDisable[9].enabled = true;
+            componentsToDisable[10].enabled = true;
+
+        }
     }
 }
