06ef8920ed4c7a0d51231792c7f919714f45837f
Reed Bell <rbel068@aucklanduni.ac.nz>
2019-09-02 02:07:16 +1200

    Added child tracking of the player model

--- Assets/Scripts/Networking/PlayerUnit.cs
+++ Assets/Scripts/Networking/PlayerUnit.cs
@@ -6,34 +6,66 @@ using UnityEngine.Networking;
 public class PlayerUnit : NetworkBehaviour
 {
     
+    [Header("Local controller only objects")]
     [SerializeField]
-    Behaviour[] componentsToDisable;
+    public Behaviour[] componentsToDisable;
+    
+    [Space(10)]
+    [Header("Root Humanoid Model object")]
+    public GameObject trackBody; 
+    
+    //Awake to create tracked child components
+    void Awake()
+    {
+        Transform[] childList = trackBody.GetComponentsInChildren<Transform>();
+
+            
+           for(int i=1; i < childList.Length; i++)
+           {
+               NetworkTransformChild newTransformChild = gameObject.AddComponent<NetworkTransformChild>(); 
+               newTransformChild.target = childList[i].transform;
+           }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         if( isLocalPlayer == false )
-            {
+        {
                 Debug.Log("PlayerUnit::Start -- Disabled components on PlayerUnit prefab");
                 for ( int i = 0; i < componentsToDisable.Length; i++ )
                 {
                     componentsToDisable[i].enabled = false;
                 }
-            }
-        
-        // So apparently the animation doesn't work unless you disable then 
-        // reenable. I am definitely doing something wrong but since it works
-        // im giving in to this shitty solution
+        }
         else
         {
-            // Disable
-            componentsToDisable[9].enabled = false;
-            componentsToDisable[10].enabled = false;
+            if(trackBody != null)
+            {
+                // So apparently the animation doesn't work unless you disable then 
+                // reenable (???). This probably has to do with the fact that I am using multiple animators
+                // on a single player mesh instead of just one single animator. However, it works for our purposes so
+                // the hacky solution stays...
 
-            // Enable
-            componentsToDisable[9].enabled = true;
-            componentsToDisable[10].enabled = true;
+                // Disable
+                componentsToDisable[9].enabled = false;
+                componentsToDisable[10].enabled = false;
 
+                // Enable
+                componentsToDisable[9].enabled = true;
+                componentsToDisable[10].enabled = true;
+
+
+                Transform[] childList = trackBody.GetComponentsInChildren<Transform>();
+
+                // Adding alot of components on a single object... not sure if it will work
+                // TODO: Think about how to do this better.
+                for(int i=1; i < childList.Length; i++)
+                {
+                    NetworkTransformChild newTransformChild = gameObject.AddComponent<NetworkTransformChild>(); 
+                    newTransformChild.target = childList[i].transform;
+                }
+            }
         }
     }
 }
