254b7bfab0e71faa5d2b3b41dea0d7c899d3236d
Reed Bell <rbel068@aucklanduni.ac.nz>
2019-09-13 21:35:15 +1200

    Added script that alters transparency of the subject

---
+++ Assets/Scripts/Networking/Ghostify.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Ghostify : NetworkBehaviour
+{
+    public GameObject ghostObject; 
+    public float alpha = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            var trans = ghostObject.GetComponent<Renderer>().material.color;
+            ghostObject.GetComponent<Renderer>().material.color = new Color(trans.r, trans.g, trans.b, alpha); 
+        }
+
+    }
+
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            var trans = ghostObject.GetComponent<Renderer>().material.color;
+            ghostObject.GetComponent<Renderer>().material.color = new Color(trans.r, trans.g, trans.b, alpha); 
+        }
+        
+    }
+    
+}
